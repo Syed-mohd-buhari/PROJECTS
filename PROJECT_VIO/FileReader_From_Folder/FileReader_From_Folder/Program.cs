5d6ccd21458db8478b1a0c3af314ed7114f85145
Syed-mohd-buhari <124652744+Syed-mohd-buhari@users.noreply.github.com>
2023-03-07 16:54:53 +0530

    Add files via upload

---
+++ PROJECT_VIO/FileReader_From_Folder/FileReader_From_Folder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileReader_From_Folder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Getting Path 
+            string Path = "C:\\Users\\syedmoha\\PROJECT_VIO\\XmlFiles";
+
+            //Calling Method & Pass the Path
+            ListFilesInDirectory(Path);
+        }
+        
+        public static void ListFilesInDirectory(string get)
+        {
+            try
+            {
+                //Getting all Files From Directory and Store in FilePath
+                string[] FilePath = Directory.GetFiles(get);
+                foreach(var Files in FilePath)
+                {
+                    Console.WriteLine(Files);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    
+    }
+}
